2866e6183d3931fce282bdc3f8a194d4491c9406
fmudrunek <filip.mudrunek@gmail.com>
2020-05-23 18:52:29 +0200

    Reworking main program to reflect changes to Github Fetcher and Slack Sender

--- src/main.py
+++ src/main.py
@@ -1,13 +1,42 @@
 from pathlib import Path
+from typing import Dict, List
 
-from notifier import fetch, send
+from notifier.pull_request_fetcher import PullRequestFetcher
+import notifier.properties as properties
+from notifier.repository import RepositoryInfo
+from notifier.slack_notifier import SlackNotifier
+from notifier.summary_formatter import RepositorySummaryFormatter
+
+# TODO rewrite to __main__.py
 
 
 def main():
-    print("STARTING!")
-    config_path = Path(__file__).parent / 'resources' / 'config.json'
-    fetch.run(config_path)
-    send.run()
+    print("Reading config")
+    config_path = Path(__file__).resolve().parent / 'resources' / 'config.json'
+    slack_repositories_config = properties.read_config(config_path)
+
+    print(f"Fetching data from {properties.get_github_base_url()}")
+    fetcher = PullRequestFetcher(properties.get_github_base_url(),
+                                 properties.get_github_token())
+
+    slack_repositories = {channel: list(map(fetcher.get_repository_info, repository_names))
+                          for (channel, repository_names) in slack_repositories_config.items()}
+    filtered = __filter_non_empty(slack_repositories)
+
+    slack_notifier = SlackNotifier("Open Pull Requests",
+                                   properties.get_slack_bearer_token(),
+                                   RepositorySummaryFormatter())
+
+    for (channel, repositories) in filtered.items():
+        slack_notifier.send_message(channel, repositories)
+
+
+def __filter_non_empty(channel_to_repository: Dict[str, List[RepositoryInfo]]) -> Dict[str, List[RepositoryInfo]]:
+    return {
+        channel: [repo for repo in repositories if repo.pulls]  # filter repositories with some PRs
+        for (channel, repositories) in channel_to_repository.items() if any(repo.pulls for repo in repositories)
+        # only add channel if it has at least one repo with PRs
+    }
 
 
 if __name__ == "__main__":
