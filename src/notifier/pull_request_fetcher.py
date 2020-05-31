7bdef92033f84d75cf401d2fb6efaca644cd530b
Filip Mudrunek <filip.mudrunek@gmail.com>
2020-05-31 22:37:15 +0200

    Nicer exception message

--- src/notifier/pull_request_fetcher.py
+++ src/notifier/pull_request_fetcher.py
@@ -7,15 +7,16 @@ from .repository import RepositoryInfo, PullRequestInfo
 class PullRequestFetcher:
 
     def __init__(self, github_url: str, token: str):
+        self.__github_url = github_url
         self.__github = Github(base_url=github_url, login_or_token=token, retry=3)
 
     def get_repository_info(self, repository_name: str) -> RepositoryInfo:
         try:
             repo = self.__github.get_repo(repository_name)
-        except UnknownObjectException as e:
-            raise ValueError(f"Failed to find repository '{repository_name}'")
-        except GithubException as ex:
-            raise ValueError(f"Failed to retrieve data from Github")
+        except UnknownObjectException:
+            raise ValueError(f"Failed to find repository '{repository_name}' in {self.__github_url}")
+        except GithubException:
+            raise ValueError(f"Failed to retrieve data from {self.__github_url}")
 
         pull_requests = repo.get_pulls(state='open', sort='created')
         return RepositoryInfo(name=repository_name,
