731abb6d0e36954d1d905ce58d84eaf7175b8d7f
fmudrunek <11849859+fmudrunek@users.noreply.github.com>
2023-09-18 21:53:32 +1000

    Ability to filter pull requests by authors (#10)

--- src/notifier/properties.py
+++ src/notifier/properties.py
@@ -1,6 +1,8 @@
 import os
 import json
-from typing import Dict, List
+from typing import Dict, List, TypeAlias
+from repository import PullRequestFilter, AuthorFilter
+from pathlib import Path
 
 
 def __get_env(env_variable: str) -> str:
@@ -21,7 +23,21 @@ def get_github_api_url() -> str:
     return __get_env("GITHUB_REST_API_URL")
 
 
-def read_config(config_path) -> Dict[str, List[str]]:
+ChannelConfig: TypeAlias = tuple[List[str], List[PullRequestFilter]]    # (repository_names, pull_request_filters)
+
+def read_config(config_path: Path) -> Dict[str, ChannelConfig]:
     with open(config_path) as json_data_file:
         config = json.load(json_data_file)
-    return {entry["slack_channel"]: entry["repositories"] for entry in config['notifications']}
+    return {entry["slack_channel"]: (entry["repositories"], __parse_filters(entry)) for entry in config['notifications']}
+
+
+def __parse_filters(config_entry) -> List[PullRequestFilter]:
+    if "pull_request_filters" not in config_entry:
+        return []
+    
+    filters = config_entry["pull_request_filters"]
+    result = []
+    if "authors" in filters:
+        result.append(AuthorFilter(filters["authors"]))
+        
+    return result
