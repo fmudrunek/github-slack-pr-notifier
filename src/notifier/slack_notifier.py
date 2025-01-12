371ec6eb73f7a1bd478007dcca53743be0254c03
Filip Mudrunek <filip.mudrunek@gmail.com>
2025-01-12 20:06:03 +1100

    Pass fetcher function to notifier

--- src/notifier/slack_notifier.py
+++ src/notifier/slack_notifier.py
@@ -1,6 +1,7 @@
 from notifier.repository import RepositoryInfo
 from notifier.summary_formatter import SummaryMessageFormatter
 from notifier.slack_client import SlackClient
+from typing import Callable
 
 
 """
@@ -11,7 +12,10 @@ class SlackBlockNotifier:
         self.client = slack_client
         self.notification_formatter = notification_formatter
 
-    def send_report_for_repos(self, channel_name: str, repositories: list[RepositoryInfo]) -> None:
+    def send_report_for_repos(self, channel_name: str, repository_names: list[str], get_repository_info: Callable[[str], RepositoryInfo]) -> None:
+        repositories = [get_repository_info(repo_name) for repo_name in repository_names]
+        if (len(repositories) == 0):
+            return
         for repo in repositories:
             messages = self.notification_formatter.get_messages_for_repo(repo)
             for message in messages:
